f6c9c2cddb4d5a28f6ee483ebfdb25dc35ca9091
Olexiy Senichek <olexiy.senichek@bureauveritas.com>
2024-06-30 14:34:37 +0200

    added stage env and swagger

--- app/main.py
+++ app/main.py
@@ -1,6 +1,28 @@
 from fastapi import FastAPI
+from fastapi.openapi.docs import get_swagger_ui_html
+from fastapi.openapi.utils import get_openapi
+import os
+
+stage_name = os.environ.get("STAGE")
 
 app = FastAPI()
+
+@app.get("/docs", include_in_schema=False)
+async def custom_swagger_ui_html():
+    return get_swagger_ui_html(
+        openapi_url=f"{stage_name}/openapi.json",
+        title="Your API Swagger UI"
+    )
+
+def custom_openapi():
+    if app.openapi_schema:
+        return app.openapi_schema
+    openapi_schema = get_openapi(
+        title="Yo API",
+        version="1.0.0",
+        description='''Test API setup' ''',
+        routes=app.routes,
+    )
 
 @app.get("/health")
 def read_root():
